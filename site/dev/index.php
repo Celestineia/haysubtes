b6fb4356a8a30dadf693a9a08e44bb803433fa8f
Chompas <chompas@gmail.com>
2012-12-05 15:20:26 -0300

    Agregada una carpeta dev para hacer pruebas mas tranquilo :)

---
+++ site/dev/index.php
@@ -0,0 +1,209 @@
+<?php
+
+function getCSS($line) {
+    $class = '';
+    switch ($line->status) {
+        case 'NORMAL':
+            $class = ' normal';
+            break;
+        case 'CANCELLED':
+            $class = ' interrumpido';
+            break;
+        case 'DELAYED':
+            $class = ' demorado';
+            break;
+        case 'SLEEPING':
+            $class = ' sleeping';
+            break;
+        case 'REDUCED':
+            $class = ' limitado';
+            break;
+        default:
+            $class = ' unknown';
+            break;
+    }
+    return $class;
+}
+
+function getStatusText($line) {
+    $status = '';
+    switch ($line->status) {
+        case 'NORMAL':
+            $status = 'Normal';
+            break;
+        case 'CANCELLED':
+            $status = 'Interrumpido';
+            break;
+        case 'DELAYED':
+            $status = 'Demorado';
+            break;
+        case 'REDUCED':
+            $status = 'Limitado';
+            break;
+        case 'SLEEPING':
+            $status = 'Durmiendo';
+            break;
+    }
+    return $status;
+}
+
+function getGlobalStatus($data) {
+    $status = 'S&iacute; :)';
+    foreach ($data as $line => $obj) {
+        if ($obj->status === 'DELAYED') {
+            $status = 'Casi :/';
+        }
+        if ($obj->status === 'REDUCED') {
+            $status = 'Casi :/';
+        }
+        if ($obj->status === 'CANCELLED') {
+            $status = 'No :(';
+            break;
+        }
+        if ($obj->status === 'SLEEPING') {
+            $status = 'Shh...';
+            break;
+        }
+    }
+    return $status;
+}
+
+function getTweetText($data) {
+    $status = '&iexcl;YAY! Todos los subtes funcionan con normalidad :D';
+    foreach ($data as $line => $obj) {
+        if ($obj->status === 'DELAYED') {
+            $status = 'Mmmh, algunos subtes andan... otros no :/';
+        }
+        if ($obj->status === 'REDUCED') {
+            $status = 'Mmmh, algunos subtes andan... otros no :/';
+        }
+        if ($obj->status === 'CANCELLED') {
+            $status = 'Buuh, todos los subtes están interrumpidos :C';
+            break;
+        }
+        if ($obj->status === 'SLEEPING') {
+            $status = '&iexcl;Oh! Los subtes est&aacute;n durmiendo';
+            break;
+        }
+    }
+    return $status;
+}
+
+$data = json_decode(file_get_contents('http://haysubtes.com/subte.php'));
+
+?><!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <title>&iquest;Hay subtes?</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.3.0/build/cssreset/reset-min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  <meta name="description" content="Enterate del estado de todas las l&iacute;neas de subtes"/>
+    <meta property="og:title" content="&iquest;Hay subtes?"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="http://www.haysubtes.com/images/fblogo.png"/>
+    <meta property="og:url" content="http://www.haysubtes.com/"/>
+    <meta property="og:site_name" content="HaySubtes.com"/>
+    <meta property="fb:app_id" content="474696555902114"/>
+  </head>
+
+  <body>
+    <!-- Facebook JS SDK -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1&appId=474696555902114";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+    <header>
+      <h1 class="titulo">&iquest;Hay subtes?</h1>
+    </header>
+
+    <div class="estado-general">
+      <?php echo getGlobalStatus((array)$data); ?>
+    </div>
+
+    <div class="lineas">
+      <ul>
+        <li class="divider"></li>
+        <li class="linea a<?php echo getCSS($data->A); ?>">
+          <div class="icono logo"></div>
+          <div class="icono estado"></div>
+          <div class="descripcion estado"><?php echo getStatusText($data->A); ?></div>
+        </li>
+        <li class="divider"></li>
+        <li class="linea b<?php echo getCSS($data->B); ?>">
+          <div class="icono logo"></div>
+          <div class="icono estado"></div>
+          <div class="descripcion estado"><?php echo getStatusText($data->B); ?></div>
+        </li>
+        <li class="divider"></li>
+        <li class="linea c<?php echo getCSS($data->C); ?>">
+          <div class="icono logo"></div>
+          <div class="icono estado"></div>
+          <div class="descripcion estado"><?php echo getStatusText($data->C); ?></div>
+        </li>
+        <li class="divider"></li>
+        <li class="linea d<?php echo getCSS($data->D); ?>">
+          <div class="icono logo"></div>
+          <div class="icono estado"></div>
+          <div class="descripcion estado"><?php echo getStatusText($data->D); ?></div>
+        </li>
+        <li class="divider"></li>
+        <li class="linea e<?php echo getCSS($data->E); ?>">
+          <div class="icono logo"></div>
+          <div class="icono estado"></div>
+          <div class="descripcion estado"><?php echo getStatusText($data->E); ?></div>
+        </li>
+        <li class="divider"></li>
+        <li class="linea h<?php echo getCSS($data->H); ?>">
+          <div class="icono logo"></div>
+          <div class="icono estado"></div>
+          <div class="descripcion estado"><?php echo getStatusText($data->H); ?></div>
+        </li>
+        <li class="divider"></li>
+        <li class="linea p<?php echo getCSS($data->P); ?>">
+          <div class="icono logo"></div>
+          <div class="icono estado"></div>
+          <div class="descripcion estado"><?php echo getStatusText($data->P); ?></div>
+        </li>
+        <li class="divider"></li>
+      </ul>
+    </div>
+
+    <footer>
+      <div class="social">
+        <a href="https://twitter.com/share" class="twitter-share-button" data-text="<?php echo getTweetText($data); ?>" data-lang="es">Twittear</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        <div class="fb-like" data-href="http://www.haysubtes.com" data-send="true" data-layout="button_count" data-width="450" data-show-faces="true"></div>
+      </div>
+
+      <div class="texto-bonito"><p>haysubtes.com se actualiza cada 5 minutos. :)</p></div>
+
+      <div class="quote"><a href="http://www.twitter.com/celestineia" target="_blank">Cerebro</a> - <a href="http://www.twitter.com/aguagraphics" target="_blank">Art</a> - <a href="http://www.twitter.com/blaquened" target="_blank">Layout</a> - <a href="http://www.twitter.com/tomasdev" target="_blank">Pinky</a></div>
+    </footer>
+  </body>
+
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36787848-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
+</html>
---
+++ site/dev/subte.php
@@ -0,0 +1,105 @@
+<?php
+
+$date = (int) date('H');
+$sleeping = false;
+
+if ($date >= 21 || $date < 3) {
+    $sleeping = true;
+}
+
+function file_get_contents_utf8($fn) {
+     $content = file_get_contents($fn);
+      return mb_convert_encoding($content, 'UTF-8',
+          mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
+}
+
+function parseMessage($str) {
+    global $sleeping;
+    if ($sleeping) {
+        return 'SLEEPING';
+    }
+    $str = strtolower($str);
+    $status = 'UNKNOWN';
+    if (strpos($str, 'servicio normal') !== false) {
+        $status = 'NORMAL';
+    } elseif (
+            (strpos($str, 'gremial') !== false) ||
+            (strpos($str, 'interrumpido') !== false) ||
+            (strpos($str, 'interrupc') !== false) ||
+            (strpos($str, 'paro') !== false)
+        ) {
+            $status = 'CANCELLED';
+    } elseif (strpos($str, 'demora') !== false) {
+        $status = 'DELAYED';
+    } elseif (strpos($str, 'limitado') !== false) {
+        $status = 'REDUCED';
+    }
+    return $status;
+}
+
+header('Content-type: application/json; charset=UTF-8');
+
+if (@$_GET['callback']) {
+    echo $_GET['callback'] . '(';
+}
+
+// Settings
+$cachedir = 'cache/';
+// every 5 mins
+$cachetime = 60 * 5;
+$cacheext = 'cache';
+// $cachepage = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+// dont cache query string
+$cachepage = $_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
+$cachefile = $cachedir.md5($cachepage).'.'.$cacheext;
+
+if (@file_exists($cachefile)) {
+    $cachelast = @filemtime($cachefile);
+} else {
+    $cachelast = 0;
+}
+@clearstatcache();
+// Mostramos el archivo si aun no vence
+if (time() - $cachetime <$cachelast) {
+    // echo 'cache hit';
+    echo file_get_contents($cachefile);
+    exit();
+}
+ob_start();
+
+$url = 'http://www.metrovias.com.ar/V2/InfoSubteSplash.asp';
+$data = file_get_contents_utf8($url);
+
+//pausecontent[\d+]\s=\s(.*);
+// pausecontent[0] = '&nbsp;&nbsp;<b>L?nea A:</b>&nbsp; Servicio normal.';pausecontent[1] =
+$results = preg_match_all('/pausecontent\[[\d+]\]\s=\s\'([^\']+)\';/', $data, $matches);
+
+$json = array();
+
+foreach($matches[1] as $line) {
+    // echo $line . "\n";
+    // $line = str_replace(array('&nbsp;', 'á', 'é', 'í', 'ó', 'ú'), array(' ', '&aacute;', '&eacute;', '&iacute;', '&oacute;', '&uacute;'), $line);
+    $line = str_replace(array('&nbsp;'), array(' '), $line);
+    // echo $line;
+    // $line = str_replace(array('&nbsp;'), array(' '), $line);
+    $line = strip_tags(html_entity_decode($line, ENT_QUOTES, 'UTF-8'));
+    // preg_match('/nea\s([A-Z]{1}):.*(\s)([A-Za-z]+)\.?$/', $line, $data);
+    preg_match('/nea\s([A-Z]{1}):\s\s?(.*)/', $line, $data);
+    // echo $line . "\n";
+    $json[$data[1]] = array(
+        'status' => parseMessage($data[2]),
+        'message' => htmlentities($data[2], ENT_QUOTES, 'UTF-8')
+    );
+    // var_dump($data);
+}
+
+echo json_encode($json);
+
+file_put_contents($cachefile, ob_get_contents()); // or var_dump(error_get_last());
+ob_end_flush();
+
+if (@$_GET['callback']) {
+    echo ');';
+}
+
+// echo $data;;
